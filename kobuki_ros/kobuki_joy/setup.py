5cec88e2ed308d0e1bceb545bffcaec209bb288d
yosoy2 <edotercero66.el@gmail.com>
2022-05-20 19:46:38 -0400

    add pkg kobuki_joy

---
+++ kobuki_ros/kobuki_joy/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'kobuki_joy'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='edu',
+    maintainer_email='edotercero66.el@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
